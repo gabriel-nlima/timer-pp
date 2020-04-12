3f2b56a1449bf49215247e74e4de257142a17519
Gabriel Nascimento Lima <gabriellima@snews.tv>
2020-04-12 15:25:22 -0300

    Usando Hermes para build Android.

--- App.tsx
+++ App.tsx
@@ -1,19 +1,23 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+declare const global: any;
+const isHermes = () => global.HermesInternal !== undefined;
+console.log(`Hermes enabled: ${isHermes()}`);
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			{isHermes() && <Text>Engine: Hermes</Text>}
+			<Text>Open up App.tsx to start working on your app!</Text>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
 });
